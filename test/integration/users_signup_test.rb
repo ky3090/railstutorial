d8eba9fb4eade3e0ac193990d6bed4c79e7b1513
katahira yuta <mgy.90.24.yu@gmail.com>
2018-10-28 20:12:21 +0900

    created user test function

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,4 +14,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert'
     assert_select 'form[action="/signup"]'
   end
+
+   test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "keisuke honda",
+                                         email: "kh@gmail.com",
+                                         password:              "12345678",
+                                         password_confirmation: "12345678" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
 end
