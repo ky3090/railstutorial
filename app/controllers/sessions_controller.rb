2bb335e633a5f8ed2c603a49d9db681a54f264c3
katahira yuta <mgy.90.24.yu@gmail.com>
2018-10-29 09:43:01 +0900

    set flash message when User can't sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      
+
     else
       # エラーメッセージを作成する
-      render 'new'
+      flash.now[:danger] = 'Invalid email/password combination'
+      render :new
     end
   end
 
