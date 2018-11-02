156a8acb92986c7e7048e91f6b213a35bfd01d92
katahira yuta <mgy.90.24.yu@gmail.com>
2018-11-02 20:45:49 +0900

    solved logout problem

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,7 +5,7 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # 現在ログイン中のユーザーを返す (いる場合)
+  # 記憶トークンcookieに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
