c042bb65944666edf2634929b418953ac25020bc
katahira yuta <mgy.90.24.yu@gmail.com>
2018-11-02 17:48:30 +0900

    ready for remember token function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,6 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to user
-
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
